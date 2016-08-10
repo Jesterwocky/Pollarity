8eee0c2977aa5bae128e4e17e2fbdd32c9150383
Jessie Walker <jekawacaneer@gmail.com>
2016-08-10 11:41:40 -0400

    try again

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -26,7 +26,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['react']
+          presets: ['react', 'es2015']
         }
       }
     ]
